41478f2186c216eea56c60b03622bf6f6ac7cc1a
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2020-03-19 11:37:33 -0300

    Add files via upload

---
+++ _site_/categoria/home.php
@@ -0,0 +1,17 @@
+
+</html>
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Home</title>
+    
+</head>
+
+<body>
+
+<h1> home</h1>
+</body>
+
+</html>
---
+++ _site_/categoria/servicos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Servicos</title>
+
+</head>
+
+<body>
+<h1>Serviços</h1>
+
+</body>
+
+</html>
